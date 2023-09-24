a9aba355866889d0a5fee70129241b4e938ac25e
Horvath Mark <horvath99mark@gmail.com>
2023-09-24 20:17:18 +0200

    Hibákat jelző kommentek hozzáadva

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //pontos változónév használata
+    std::cout << '1-100 ertekek duplazasa' // aposztróf helyett idézőjel, illetve hiányzó pontosvessző
+    for (int i = 0;) //hiányzó feltétel, ciklusváltozó növelése
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //hiányzó feltétel
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //hiányzó pontosvessző, érték kiírásának pótlása
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; //értékadás
+    for (int i = 0; i < N_ELEMENTS, i++) //vessző lecserélése pontosvesszőre
     {
-        atlag += b[i]
+        atlag += b[i] //hiányzó pontosvessző
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    //memóriát fel kell szabadítani
     return 0;
 }
