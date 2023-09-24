a3e798a16e15773493a146741ef8dad310f6bad0
Horvath Mark <horvath99mark@gmail.com>
2023-09-24 20:36:48 +0200

    Hibák kijavítva

--- main.cpp
+++ main.cpp
@@ -4,24 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //pontos változónév használata
-    std::cout << '1-100 ertekek duplazasa' // aposztróf helyett idézőjel, illetve hiányzó pontosvessző
-    for (int i = 0;) //hiányzó feltétel, ciklusváltozó növelése
+    int *b = new int[N_ELEMENTS]; //pontos változónév használata
+    std::cout << "1-100 ertekek duplazasa" << std::endl; // aposztróf helyett idézőjel, illetve hiányzó pontosvessző és sortörés
+    for (int i = 0; i < N_ELEMENTS; i++) //hiányzó feltétel, ciklusváltozó növelése
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) //hiányzó feltétel
+    for (int i = 0; i < N_ELEMENTS; i++) //hiányzó feltétel
     {
-        std::cout << "Ertek:" //hiányzó pontosvessző, érték kiírásának pótlása
+        std::cout << "Ertek: " << b[i] << std::endl; //hiányzó pontosvessző, érték kiírásának pótlása
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; //értékadás
-    for (int i = 0; i < N_ELEMENTS, i++) //vessző lecserélése pontosvesszőre
+    int atlag = 0; //értékadás
+    for (int i = 0; i < N_ELEMENTS; i++) //vessző lecserélése pontosvesszőre
     {
-        atlag += b[i] //hiányzó pontosvessző
+        atlag += b[i]; //hiányzó pontosvessző
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    //memóriát fel kell szabadítani
+    delete[] b; //memóriát fel kell szabadítani 
     return 0;
 }
